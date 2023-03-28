a62ba1022c2e35479ab577fbe4455f3a4be675f6
Hafiz Muhammad Attaullah <attaullahshafiq10@gmail.com>
2023-03-28 18:03:36 +0500

    Create areaofsq.m

---
+++ Numerical Computing - 2023/MATLAB Codes/function file/areaofsq.m
@@ -0,0 +1,10 @@
+% example 1: Area of square as an output
+
+function [A]=areaofsq(x)
+A = x*x;       %formula: area of square
+end
+
+
+
+% now save the file and run it in command line
+% Area = areaofsq(5)
